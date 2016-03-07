4a8b0bc0b3b7ba7204ce8c8b4c6c53cd0ff1cb8b
oldtimerza <bradleymorrisza@gmail.com>
2016-03-07 22:34:30 +0200

    FF3-Started adding Layout-BM Reviewer:BM

--- fogforge/lib/routes.jsx
+++ fogforge/lib/routes.jsx
@@ -4,3 +4,11 @@ FlowRouter.route("/", {
         ReactLayout.render(Home);
     }
 });
+
+function renderMainLayout(component){
+    ReactLayout.render(MainLayout, {
+        header: <Header />,
+        content: component,
+        footer: <Footer />
+    })
+}
